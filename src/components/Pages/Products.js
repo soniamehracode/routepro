da486447dd4b5a902969c28c96321b921d5ed904
Sonia Mehra <soniamehra.work@gmail.com>
2021-02-21 13:37:29 +0530

    change 5

--- src/components/Pages/Products.js
+++ src/components/Pages/Products.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import '../../App.css'
 export default function Products() {
-  return <h1 className="products">PRODUCTS 2</h1>
+  return <h1 className="products">PRODUCTS 4</h1>
 }
